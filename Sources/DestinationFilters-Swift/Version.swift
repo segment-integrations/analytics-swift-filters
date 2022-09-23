97e6035660b99ef665d5fed30f9ea099137e719b
Prayansh Srivastava <prayansh@gmail.com>
2022-09-23 12:19:19 -0700

    remove idea and add release docs

---
+++ Sources/DestinationFilters-Swift/Version.swift
@@ -0,0 +1,10 @@
+//
+//  Version.swift
+//
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// Use release.sh's automation.
+// BREAKING.FEATURE.FIX
+internal let __destinationFilters_version = "1.0.0"
