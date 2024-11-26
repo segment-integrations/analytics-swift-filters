952c5159ffa84db198a56f241da7162c13d2b4be
Brandon Sneed <brandon@redf.net>
2024-11-26 13:01:33 -0800

    Bumped analytics swift & live versions to preserve compat.

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/segment-integrations/analytics-swift-live.git", from: "3.1.0"),
-        .package(url: "https://github.com/segmentio/analytics-swift.git", from: "1.4.7"),
+        .package(url: "https://github.com/segment-integrations/analytics-swift-live.git", from: "3.1.1"),
+        .package(url: "https://github.com/segmentio/analytics-swift.git", from: "1.6.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
